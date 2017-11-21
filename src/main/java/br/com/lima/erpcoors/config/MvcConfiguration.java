f95f7ea0244f7095eea5d7cad7cc6247b4d931a8
Everton Lima Aleixo <everton.jiujitsu@gmail.com>
2017-11-21 18:05:34 -0200

    Adding index. Issue: does not load css and js.

---
+++ src/main/java/br/com/lima/erpcoors/config/MvcConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.lima.erpcoors.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration  extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println("reached here ");
+        registry.addResourceHandler("/resources/**").addResourceLocations("/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+}
---
+++ src/main/java/br/com/lima/erpcoors/controller/IndexController.java
@@ -0,0 +1,24 @@
+package br.com.lima.erpcoors.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping(path = { "/", "/index", "/home" })
+	public String index() {
+		return "/index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
+
+	@GetMapping("/403")
+	public String error403() {
+		return "/error/403";
+	}
+
+}
