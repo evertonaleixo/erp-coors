97f36a819206196e3d9ee9089400db1651210b83
Everton <everton.jiujitsu@gmail.com>
2017-11-21 00:32:12 +0000

    creating coors project in springboot

---
+++ src/main/java/br/com/lima/erpcoors/ErpCoorsApplication.java
@@ -0,0 +1,12 @@
+package br.com.lima.erpcoors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpCoorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpCoorsApplication.class, args);
+	}
+}
