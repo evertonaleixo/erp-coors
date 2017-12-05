0f52129ce1b0010c4579039c59fb72847a99a83b
Everton <everton.jiujitsu@gmail.com>
2017-12-05 00:52:51 +0000

    crud with Clients

--- src/main/java/br/com/lima/erpcoors/controller/ClientController.java
+++ src/main/java/br/com/lima/erpcoors/controller/ClientController.java
@@ -1,18 +1,61 @@
 package br.com.lima.erpcoors.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.lima.erpcoors.model.Client;
+import br.com.lima.erpcoors.repository.ClientRepository;
+
 @Controller
+@RequestMapping("/clientes")
 public class ClientController {
 	
-	@GetMapping("/clientes")
+	@Autowired
+	ClientRepository clients;
+	
+	@GetMapping("/listar")
 	public ModelAndView clientes() {
 		ModelAndView mav = new ModelAndView("clientes");
-
+		mav.addObject("clients", clients.findAll());
+		
 		return mav;
 	}
 	
+	@GetMapping("/{id}")
+	public ModelAndView updateClient(@PathVariable("id") Long id) {
+		Client one = clients.findOne(id);
+		
+		return getFicha(one);
+	}
+	
+	@GetMapping("/del/{id}")
+	public ModelAndView deleteClient(@PathVariable("id") Long id) {
+		Client one = clients.findOne(id);
+		
+		if(one != null && one.getId() != 0)
+			clients.delete(one);
+		
+		return clientes();
+	}
+	
+	@GetMapping("/novo")
+	public ModelAndView getFicha(Client client) {
+		ModelAndView mav = new ModelAndView("clientes_ficha");
+		mav.addObject("client", client);
+		
+		return mav;
+	}
+	
+	@PostMapping("/novo")
+	public ModelAndView newClient(Client client) {
+		client = clients.saveAndFlush(client);
+		
+		return clientes();
+	}
 	
 }
--- src/main/java/br/com/lima/erpcoors/model/Client.java
+++ src/main/java/br/com/lima/erpcoors/model/Client.java
@@ -5,12 +5,15 @@ import java.util.List;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
 public class Client {
 	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private long id;
 	
 	private String cpf_cnpj;
@@ -22,4 +25,53 @@ public class Client {
 	
 	@OneToMany
 	private List<Order> orders;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCpf_cnpj() {
+		return cpf_cnpj;
+	}
+
+	public void setCpf_cnpj(String cpf_cnpj) {
+		this.cpf_cnpj = cpf_cnpj;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(List<String> phones) {
+		this.phones = phones;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
 }
