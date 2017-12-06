05cab98fc8ede51483685ee1dc712869981438ff
Everton Lima Aleixo <everton.jiujitsu@gmail.com>
2017-12-06 15:44:34 -0200

    Add Phone Entity.

---
+++ src/main/java/br/com/lima/erpcoors/model/Phone.java
@@ -0,0 +1,36 @@
+package br.com.lima.erpcoors.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Phone {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	
+	private String phone;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return this.phone;
+	}
+}
