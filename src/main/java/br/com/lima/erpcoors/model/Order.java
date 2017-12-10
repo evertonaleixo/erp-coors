8de449f525a2deaa934507e617fab1a5328e160b
Everton Lima Aleixo <everton.jiujitsu@gmail.com>
2017-12-09 23:30:16 -0200

    Fixing headers.

--- src/main/java/br/com/lima/erpcoors/model/Order.java
+++ src/main/java/br/com/lima/erpcoors/model/Order.java
@@ -1,16 +1,92 @@
 package br.com.lima.erpcoors.model;
 
+import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Order {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
-	
-	private Date createdAt;
-	
+
+	private Date created;
+
+	@OneToOne(cascade = CascadeType.DETACH, mappedBy="Order")
+	private Client client;
+
+	@OneToMany(cascade = CascadeType.DETACH)
+	private List<Product> products;
+
+	private BigDecimal discount;
+
+	private BigDecimal fullValue;
+
+	private BigDecimal value;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public BigDecimal getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(BigDecimal discount) {
+		this.discount = discount;
+	}
+
+	public BigDecimal getFullValue() {
+		return fullValue;
+	}
+
+	public void setFullValue(BigDecimal fullValue) {
+		this.fullValue = fullValue;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
 }
