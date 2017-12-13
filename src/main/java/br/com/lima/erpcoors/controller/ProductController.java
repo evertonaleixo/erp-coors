ebc61140648fdbbd134be6e406dbf6d319854deb
Everton Lima Aleixo <everton.jiujitsu@gmail.com>
2017-12-13 08:50:56 -0200

    Refactoring ProductController.

--- src/main/java/br/com/lima/erpcoors/controller/ProductController.java
+++ src/main/java/br/com/lima/erpcoors/controller/ProductController.java
@@ -40,7 +40,7 @@ public class ProductController {
 	}
 
 	@GetMapping("/listar")
-	public ModelAndView produtos() {
+	public ModelAndView list() {
 		ModelAndView mav = new ModelAndView("produtos");
 		mav.addObject("products", products.findAll());
 
@@ -48,20 +48,20 @@ public class ProductController {
 	}
 
 	@GetMapping("/{id}")
-	public ModelAndView updateClient(@PathVariable("id") Long id) {
+	public ModelAndView updateProduct(@PathVariable("id") Long id) {
 		Product one = products.findOne(id);
 
 		return getFicha(one);
 	}
 
 	@PostMapping(value = "/novo", params = { "newProd" })
-	public ModelAndView newClient(Product product) {
+	public ModelAndView newProduct(Product product) {
 		if (product.getId() < 0)
 			product.setId(0);
 
 		product = products.save(product);
 
-		return produtos();
+		return list();
 	}
 
 	@GetMapping("/novo")
@@ -79,6 +79,6 @@ public class ProductController {
 		if (one != null && one.getId() != 0)
 			products.delete(one);
 
-		return produtos();
+		return list();
 	}
 }
