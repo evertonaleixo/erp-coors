4874919baf33258ae718271516d5a7a867e464e8
Everton <everton.jiujitsu@gmail.com>
2018-01-03 22:38:00 +0000

    Fix routes.

--- src/main/java/br/com/lima/erpcoors/controller/IndexController.java
+++ src/main/java/br/com/lima/erpcoors/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping(path = { "/" })
+	@GetMapping(path = { "/home" })
 	public String index() {
 		return "/index";
 	}
