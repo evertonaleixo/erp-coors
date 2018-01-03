ca984d7762c6bf2a5d5b40354fcb0f9b06c66edc
Everton <everton.jiujitsu@gmail.com>
2018-01-03 23:57:01 +0000

    Fix routes.

--- src/main/java/br/com/lima/erpcoors/config/MvcConfiguration.java
+++ src/main/java/br/com/lima/erpcoors/config/MvcConfiguration.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 // @EnableWebMvc --> Not use this annotation. If you use, the WebMVC will force
 // static-files resource configuration, and you lose access.
 public class MvcConfiguration extends WebMvcConfigurerAdapter {
+	private boolean dev = false;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static").addResourceLocations("classpath:/static/");
@@ -26,11 +28,25 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean(name = "dataSource")
 	public DriverManagerDataSource dataSource() {
+		
+		
+		String usrName = "lphhmajmocspmx";
+		String pwd = "457580ed808a085833e476a147dec1d043ac27b1fff7fcb29196e04d4042be5d";
+		String url = "jdbc:postgresql://ec2-54-235-85-127.compute-1.amazonaws.com:5432/de1lr14lshg058";
+		String driver = "org.postgresql.Driver";
+
+		if(dev) {
+			usrName = "root";
+			pwd = "test";
+			url = "jdbc:mysql://localhost/coors";
+			driver = "com.mysql.jdbc.Driver";
+		}
+		
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-		driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
-		driverManagerDataSource.setUrl("jdbc:postgresql://ec2-54-235-85-127.compute-1.amazonaws.com:5432/de1lr14lshg058");
-		driverManagerDataSource.setUsername("lphhmajmocspmx");
-		driverManagerDataSource.setPassword("457580ed808a085833e476a147dec1d043ac27b1fff7fcb29196e04d4042be5d");
+		driverManagerDataSource.setDriverClassName(driver);
+		driverManagerDataSource.setUrl(url);
+		driverManagerDataSource.setUsername(usrName);
+		driverManagerDataSource.setPassword(pwd);
 		return driverManagerDataSource;
 	}
 }
